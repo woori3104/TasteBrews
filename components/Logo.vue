4ceb0099102680a59439d3ae1715500a3dbb7d48
woori <woori.kim.se@gmail.com>
2023-09-10 08:41:07 +0900

    header

---
+++ components/Logo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="centered-image">
+    <img class="logo" src="../assets/beer.png" />
+  </div>
+</template>
+
+<style>
+.centered-image {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo {
+  width: 100px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import logo from "../components/Logo.vue";
+import mainTitle from "../components/MainTitle.vue";
+import { ref } from "vue";
+import axios from "axios";
+const location = ref<{ latitude: number; longitude: number } | null>(null);
+const country = ref<string>("");
+
+const getBreweriesInCurrentLocation = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.openbrewerydb.org/breweries?by_country=${country.value}`
+    );
+    const data = response.data;
+    country.value = "";
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <logo />
+    <mainTitle />
+    <div class="body" />
+  </div>
+</template>
+
+<style>
+/*스타일링. */
+</style>
